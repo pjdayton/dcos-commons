64e9babfb354b4c087dcd87859beb7463d2ddeb6
Andrei Dragomir <adragomi@adobe.com>
2017-09-11 13:54:52 +0300

    Implement simple secret passing through the framework

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/specification/ServiceSpec.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/specification/ServiceSpec.java
@@ -19,6 +19,9 @@ public interface ServiceSpec extends Configuration {
     @JsonProperty("principal")
     String getPrincipal();
 
+    @JsonProperty("secret")
+    String getSecret();
+
     @JsonProperty("pod-specs")
     List<PodSpec> getPods();
 
